418e8bd68903b978a9cede5332c4d1c82198686f
danilo <dl539994@gmail.com>
2019-08-13 15:58:19 -0300

    arrumando salario

---
+++ funcionario1.py
@@ -0,0 +1,7 @@
+nome = input("digite seu nome: ")
+horas = float (input("digite suas horas de trabalho: "))
+valor = float(input("digite o valor de cada hora: "))
+
+
+salario = horas * valor
+print(str(nome) + " seu salario é:" + " R$" str(salario) + " reais" + " dolar")
